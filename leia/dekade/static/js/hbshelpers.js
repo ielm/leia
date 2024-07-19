3b8163c039f8eb773cee943e3943b35c387cf4cc
ielm <i.leonmaldonado@gmail.com>
2024-07-19 10:05:10 -0400

    clickable links in ontology fillers

--- leia/dekade/static/js/hbshelpers.js
+++ leia/dekade/static/js/hbshelpers.js
@@ -6,6 +6,10 @@ Handlebars.registerHelper("json", function(context) {
     return JSON.stringify(context);
 });
 
+Handlebars.registerHelper("ifeq", function(arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
+
 Handlebars.registerHelper("eachCandidateScoreParameter", function(score, options) {
     const copiedScore = {};
     Object.assign(copiedScore, score);
