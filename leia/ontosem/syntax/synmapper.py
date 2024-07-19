58515a9adfa28a24222ce2d6d956b39a489a18a3
ielm <i.leonmaldonado@gmail.com>
2024-07-19 10:05:11 -0400

    (wip) defined synmatcher (generic synstruc pattern matcher)

--- leia/ontosem/syntax/synmapper.py
+++ leia/ontosem/syntax/synmapper.py
@@ -1,7 +1,10 @@
+from dataclasses import dataclass
+from leia.ontomem.lexicon import SynStruc
 from leia.ontomem.ontology import Ontology
 from leia.ontosem.analysis import WMLexicon
 from leia.ontosem.config import OntoSemConfig
-from leia.ontosem.syntax.results import SynMap, Syntax
+from leia.ontosem.syntax.results import ConstituencyNode, Dependency, SynMap, Syntax, Word
+from typing import List
 
 
 class SynMapper(object):
@@ -13,3 +16,60 @@ class SynMapper(object):
 
     def run(self, syntax: Syntax) -> SynMap:
         raise NotImplementedError
+
+
+class SynMatcher(object):
+
+    """
+    The job of the SynMatcher is to find any matches between an input SynStruc definition and the current syntactic
+    analysis.  That is, if the SynStruc demands certain dependencies, constituencies, or tokens, the SynMatcher
+    will attempt to identify and align those components in the input syntax.
+
+    The SynMatcher respects the order of elements in the SynStruc.
+
+    A specified root token may be optionally provided.
+    """
+
+    class SynMatch:
+        pass
+
+    @dataclass
+    class RootMatch(SynMatch):
+        element: SynStruc.RootElement
+        match: Word
+
+    @dataclass
+    class TokenMatch(SynMatch):
+        element: SynStruc.TokenElement
+        match: Word
+
+    @dataclass
+    class DependencyMatch(SynMatch):
+        element: SynStruc.DependencyElement
+        match: Dependency
+
+    @dataclass
+    class ConstituencyMatch(SynMatch):
+        element: SynStruc.ConstituencyElement
+        match: ConstituencyNode
+
+    @dataclass
+    class SynMatchResult(object):
+        matches: List['SynMatcher.SynMatch']
+
+    def __init__(self, config: OntoSemConfig, ontology: Ontology, lexicon: WMLexicon):
+        self.config = config
+        self.ontology = ontology
+        self.lexicon = lexicon
+
+    def run(self, syntax: Syntax, synstruc: SynStruc, root: Word=None) -> List['SynMatcher.SynMatchResult']:
+        raise NotImplementedError
+
+    # [
+    #     {"type": "dependency", "deptype": "nsubjpass", "governor": 0}
+    # ],
+    # [
+    #     {"type": "constituency", "contype": "NP", "children": []},
+    #     {"type": "token", "lemma": ["be"], "pos": null, "morph": {}},
+    #     {"type": "token", "lemma": [], "pos": "V", "var": 0, "morph": {"tense": "past", "verbform": "part"}}
+    # ]
--- leia/tests/ontosem/syntax/SynMapperTestCase.py
+++ leia/tests/ontosem/syntax/SynMapperTestCase.py
@@ -1,9 +1,15 @@
 from leia.ontomem.memory import Memory
-from leia.ontosem.syntax.synmapper import SynMapper
+from leia.ontosem.syntax.synmapper import SynMatcher, SynMapper
 from leia.tests.LEIATestCase import LEIATestCase
 
 
 class SynMapperTestCase(LEIATestCase):
+
+    def setUp(self):
+        self.m = Memory("", "", "")
+
+
+class SynMatcherTestCase(LEIATestCase):
 
     def setUp(self):
         self.m = Memory("", "", "")
