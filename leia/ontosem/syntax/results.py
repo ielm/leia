dd411045149881014ef6ed1f7cbd9e2d18f77ab2
ielm <i.leonmaldonado@gmail.com>
2024-07-19 10:05:11 -0400

    (wip) parsing spacy into leia objects

--- leia/ontosem/syntax/results.py
+++ leia/ontosem/syntax/results.py
@@ -1,6 +1,8 @@
+from coreferee.data_model import Chain
 from enum import Enum
 from leia.ontomem.lexicon import Sense
 from pyparsing import OneOrMore, nestedExpr
+from spacy.tokens import Span, Token
 from typing import Dict, List, Union
 
 
@@ -40,6 +42,96 @@ class Syntax(object):
             original_sentence = original_sentence[1:-1]
 
         return Syntax(words, synmap, lex_senses, sentence, original_sentence, parse, basic_deps, enhanced_deps)
+
+    @classmethod
+    def from_spacy(cls, sentence: Span) -> 'Syntax':
+
+        def _ner_span(token: Token) -> Union[Span, None]:
+            for entity in sentence.doc.ents:
+                if token in entity:
+                    return entity
+            return None
+
+        def _coref_chain(token: Token) -> Union[Chain, None]:
+            for chain in sentence.doc._.coref_chains:
+                for mention in chain.mentions:
+                    if mention.root_index == token.i:
+                        return chain
+            return None
+
+        words = list(map(lambda token: Word.from_spacy(token, _ner_span(token), _coref_chain(token)), sentence.subtree))
+
+
+        # print("\nDEPENDENCIES:")
+        # for sent in doc.sents:
+        #     for token in sent.subtree:
+        #         print("GOV(%s-%d)  -[%s]->  DEP(%s-%d)" % (
+        #         token.head.text, token.head.i, token.dep_, token.text, token.i))
+        #
+        # print("\nNOUN PHRASES:")
+        # for chunk in doc.noun_chunks:
+        #     print(list(map(lambda token: "%s-%d" % (token.text, token.i), chunk)))
+        #
+        # def _format_constituency_parse(node, depth):
+        #     indent = "  ".join(map(lambda i: "", range(depth + 1)))
+        #
+        #     if len(node._.labels) > 0:
+        #         print("%s%s" % (indent, node._.labels[0]))
+        #     else:
+        #         print("%s%s %s-%d" % (indent, node.root.tag_, node.root.text, node.root.i))
+        #     for child in node._.children:
+        #         _format_constituency_parse(child, depth + 1)
+        #
+        # class TreeNode(object):
+        #
+        #     def __init__(self, label: str):
+        #         self.label = label
+        #         self.children = []
+        #
+        #     def pretty_print(self, depth: int = 0):
+        #         indent = "  ".join(map(lambda i: "", range(depth + 1)))
+        #         print("%s%s" % (indent, self.label))
+        #         for child in self.children:
+        #             if isinstance(child, TreeNode):
+        #                 child.pretty_print(depth=depth + 1)
+        #             else:
+        #                 indent = "  ".join(map(lambda i: "", range(depth + 2)))
+        #                 print("%s%s-%d" % (indent, child.text, child.i))
+        #
+        # from pyparsing import OneOrMore, nestedExpr
+        #
+        # def _parse_constituencies(sentence, tree: str):
+        #
+        #     data = OneOrMore(nestedExpr()).parseString(tree)
+        #     data = data.asList()[0]
+        #
+        #     token_index = 0
+        #
+        #     def _to_nodes(parsed, parent):
+        #         nonlocal token_index
+        #
+        #         if len(parsed) > 1 or not isinstance(parsed[0], str):
+        #             for p in parsed:
+        #                 node = TreeNode(p[0])
+        #                 _to_nodes(p[1:], node)
+        #                 parent.children.append(node)
+        #         else:
+        #             parent.children.append(sentence[token_index])
+        #             token_index += 1
+        #
+        #     node = TreeNode(data[0])
+        #     _to_nodes(data[1:], node)
+        #
+        #     return node
+        #
+        # print("\nCONSTITUENCY PARSES:")
+        # for sent in doc.sents:
+        #     print(sent._.parse_string)
+        #     # _format_constituency_parse(sent, 0)
+        #     _parse_constituencies(sent, sent._.parse_string).pretty_print()
+
+        return Syntax(words, None, None, None, None, None, None, None)
+
 
     def __init__(self, words: List['Word'], synmap: 'SynMap', lex_senses: List['Sense'], sentence: str, original_sentence: str, parse: list, basic_deps: list, enhanced_deps: list):
         self.words = words
@@ -127,44 +219,67 @@ class SenseMap(object):
 
 class Word(object):
 
+    # class Ner(Enum):
+    #     CAUSE_OF_DEATH = "CAUSE_OF_DEATH"
+    #     CITY = "CITY"
+    #     COUNTRY = "COUNTRY"
+    #     CRIMINAL_CHARGE = "CRIMINAL_CHARGE"
+    #     DATE = "DATE"
+    #     DURATION = "DURATION"
+    #     EMAIL = "EMAIL"
+    #     HANDLE = "HANDLE"
+    #     IDEOLOGY = "IDEOLOGY"
+    #     LOCATION = "LOCATION"
+    #     MISC = "MISC"
+    #     MONEY = "MONEY"
+    #     NONE = "O"
+    #     NATIONALITY = "NATIONALITY"
+    #     NUMBER = "NUMBER"
+    #     ORDINAL = "ORDINAL"
+    #     ORGANIZATION = "ORGANIZATION"
+    #     PERCENT = "PERCENT"
+    #     PERSON = "PERSON"
+    #     RELIGION = "RELIGION"
+    #     SET = "SET"
+    #     STATE_OR_PROVINCE = "STATE_OR_PROVINCE"
+    #     TIME = "TIME"
+    #     TITLE = "TITLE"
+    #     URL = "URL"
+
     class Ner(Enum):
-        CAUSE_OF_DEATH = "CAUSE_OF_DEATH"
-        CITY = "CITY"
-        COUNTRY = "COUNTRY"
-        CRIMINAL_CHARGE = "CRIMINAL_CHARGE"
+        CARDINAL = "CARDINAL"
         DATE = "DATE"
-        DURATION = "DURATION"
-        EMAIL = "EMAIL"
-        HANDLE = "HANDLE"
-        IDEOLOGY = "IDEOLOGY"
-        LOCATION = "LOCATION"
-        MISC = "MISC"
+        EVENT = "EVENT"
+        FAC = "FAC"
+        GPE = "GPE"
+        LANGUAGE = "LANGUAGE"
+        LAW = "LAW"
+        LOC = "LOC"
         MONEY = "MONEY"
-        NONE = "O"
-        NATIONALITY = "NATIONALITY"
-        NUMBER = "NUMBER"
+        NONE = "NONE"
+        NORP = "NORP"
         ORDINAL = "ORDINAL"
-        ORGANIZATION = "ORGANIZATION"
+        ORG = "ORG"
         PERCENT = "PERCENT"
         PERSON = "PERSON"
-        RELIGION = "RELIGION"
-        SET = "SET"
-        STATE_OR_PROVINCE = "STATE_OR_PROVINCE"
+        PRODUCT = "PRODUCT"
+        QUANTITY = "QUANTITY"
         TIME = "TIME"
-        TITLE = "TITLE"
-        URL = "URL"
+        WORK_OF_ART = "WORK_OF_ART"
 
 
     @classmethod
-    def basic(cls, index: int, lemma: str="", pos: List[str]=None, token: str="", char_start: int=-1, char_end: int=-1, ner: 'Word.Ner'=None, coref: List['WordCoreference']=None) -> 'Word':
+    def basic(cls, index: int, lemma: str="", pos: List[str]=None, token: str="", char_start: int=-1, char_end: int=-1, ner: 'Word.Ner'=None, coref: List['WordCoreference']=None, morphology: Dict[str, str]=None) -> 'Word':
         if pos is None:
             pos = list()
         if ner is None:
             ner = Word.Ner.NONE
         if coref is None:
             coref = list()
+        if morphology is None:
+            morphology = dict()
 
-        return Word(index, lemma, pos, token, char_start, char_end, ner, coref)
+        return Word(index, lemma, pos, token, char_start, char_end, ner, coref, morphology)
 
     @classmethod
     def parse_lisp_results(cls, lisp: list) -> 'Word':
@@ -181,9 +296,37 @@ class Word(object):
         if token.startswith("\"") and token.endswith("\""):
             token = token[1:-1]
 
-        return Word(index, lemma, pos, token, char_start, char_end, ner, coref)
+        return Word(index, lemma, pos, token, char_start, char_end, ner, coref, {})
 
-    def __init__(self, index: int, lemma: str, pos: List[str], token: str, char_start: int, char_end: int, ner: 'Word.Ner', coref: List['WordCoreference']):
+    @classmethod
+    def from_spacy(cls, token: Token, ner: Union[Span, None], coref: Union[Chain, None]) -> 'Word':
+
+        ner = Word.Ner.NONE if ner is None else Word.Ner(ner.label_)
+
+        coref = [] if coref is None else list(
+            map(lambda index: WordCoreference(0, index, 0.9),
+                filter(lambda index: index != token.i,
+                    map(lambda mention: mention.root_index, coref.mentions))
+            )
+        )
+
+        # Note: The coreference index is relative to the first sentence (hence sentencenum = 0).
+        # Consider recalculating to find the sentence index and the word's index relative to that sentence
+        # if needed.
+
+        return Word(
+            token.i,
+            token.lemma_,
+            [token.pos_, token.tag_],
+            token.text,
+            token.idx,
+            token.idx + len(token.text),
+            ner,
+            coref,
+            token.morph.to_dict()
+        )
+
+    def __init__(self, index: int, lemma: str, pos: List[str], token: str, char_start: int, char_end: int, ner: 'Word.Ner', coref: List['WordCoreference'], morphology: Dict[str, str]):
         self.index = index
         self.lemma = lemma
         self.pos = pos
@@ -192,6 +335,7 @@ class Word(object):
         self.char_end = char_end
         self.ner = ner
         self.coref = coref
+        self.morphology = morphology
 
     def to_dict(self) -> dict:
         return {
@@ -202,7 +346,8 @@ class Word(object):
             "char-start": self.char_start,
             "char-end": self.char_end,
             "ner": self.ner.name,
-            "coref": list(map(lambda cr: cr.to_dict(), self.coref))
+            "coref": list(map(lambda cr: cr.to_dict(), self.coref)),
+            "morphology": self.morphology
         }
 
     def __repr__(self):
