a0d3c90386583c527d7c49c7e6778b9b7ecaa38a
ielm <i.leonmaldonado@gmail.com>
2024-07-19 10:05:07 -0400

    defining custom content link element

--- leia/dekade/static/js/main.js
+++ leia/dekade/static/js/main.js
@@ -16,4 +16,24 @@ class HelpButton extends HTMLButtonElement {
 
 }
 
+class ContentLink extends HTMLAnchorElement {
+
+    constructor() {
+        super();
+        this.addEventListener("click", e => { this._onClicked(e) });
+    }
+
+    _onClicked(event) {
+        const contentId = $(event.currentTarget).data("content-id");
+
+        if (!event.altKey) {
+            console.log("Open " + contentId + " and display it.");
+        } else {
+            console.log("Open " + contentId + " but do not display it, unless it is the only content.")
+        }
+    }
+
+}
+
 customElements.define("help-button", HelpButton, { extends: "button" });
+customElements.define("content-link", ContentLink, { extends: "a" });
