59a1652bddc01f1e4009ef6a027859f9b40feedd
ielm <i.leonmaldonado@gmail.com>
2024-07-19 10:06:29 -0400

    synstruc rendering

--- leia/dekade/static/js/hbshelpers.js
+++ leia/dekade/static/js/hbshelpers.js
@@ -10,8 +10,27 @@ Handlebars.registerHelper("ifeq", function(arg1, arg2, options) {
     return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
 });
 
+Handlebars.registerHelper("ifempty", function(arg, options) {
+    return (arg == {}) || (arg == []) || (arg == "") ? options.fn(this): options.inverse(this);
+});
+
 Handlebars.registerHelper("toUpperCase", function(str) {
     return str.toUpperCase();
+});
+
+Handlebars.registerHelper({
+    eq: (v1, v2) => v1 === v2,
+    ne: (v1, v2) => v1 !== v2,
+    lt: (v1, v2) => v1 < v2,
+    gt: (v1, v2) => v1 > v2,
+    lte: (v1, v2) => v1 <= v2,
+    gte: (v1, v2) => v1 >= v2,
+    and() {
+        return Array.prototype.every.call(arguments, Boolean);
+    },
+    or() {
+        return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+    }
 });
 
 Handlebars.registerHelper("eachCandidateScoreParameter", function(score, options) {
