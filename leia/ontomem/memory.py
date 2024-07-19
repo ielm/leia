72d263dc56788d20a91d0ac766c6c061c0db6be3
ielm <i.leonmaldonado@gmail.com>
2024-07-19 10:05:09 -0400

    added generic edit buffers to memory

--- leia/ontomem/memory.py
+++ leia/ontomem/memory.py
@@ -1,5 +1,6 @@
 from multiprocessing import Process
 from threading import Thread
+from typing import Set
 
 import functools
 import json
@@ -10,6 +11,13 @@ import string
 
 
 class Memory(object):
+
+    class EditBuffers(object):
+
+        def __init__(self, memory: 'Memory'):
+            self.ontology = OntoMemEditBuffer(memory)
+            self.properties = OntoMemEditBuffer(memory)
+            self.lexicon = OntoMemEditBuffer(memory)
 
     def __init__(self, props_path: str, ont_path: str, lex_path: str):
         from leia.ontomem.episodic import Space
@@ -22,6 +30,7 @@ class Memory(object):
         self.properties = PropertyInventory(self, props_path, load_now=False)
         self.ontology = Ontology(self, ont_path, load_now=False)
 
+        self.edits = Memory.EditBuffers(self)
         self._server: OntoMemTCPServer = None
 
     def start_tcp_server(self, host: str="", port: int=5999):
@@ -34,6 +43,31 @@ class Memory(object):
         if self._server is not None:
             self._server.shutdown()
             self._server = None
+
+
+class OntoMemEditBuffer(object):
+
+    def __init__(self, memory: Memory):
+        self.memory = memory
+        self.edits = dict()
+
+    def edited(self, source: str=None) -> Set[str]:
+        if source is not None:
+            return set(map(lambda x: x[0], filter(lambda i: source in i[1], self.edits.items())))
+
+        return set(self.edits.keys())
+
+    def note_edited(self, identifier: str, source: str):
+        if identifier not in self.edits:
+            self.edits[identifier] = set()
+
+        self.edits[identifier].add(source)
+
+    def clear(self, identifier: str=None):
+        if identifier is not None:
+            del self.edits[identifier]
+        else:
+            self.edits = dict()
 
 
 class OntoMemTCPServer(socketserver.TCPServer):
--- leia/tests/ontomem/MemoryTestCase.py
+++ leia/tests/ontomem/MemoryTestCase.py
@@ -1,9 +1,38 @@
-from leia.ontomem.memory import Memory, OntoMemTCPClient, OntoMemTCPRequest, OntoMemTCPServer
+from leia.ontomem.memory import Memory, OntoMemEditBuffer, OntoMemTCPClient, OntoMemTCPRequest, OntoMemTCPServer
 from leia.ontomem.memory import OntoMemTCPRequestGetInstance, OntoMemTCPRequestGetSense, OntoMemTCPRequestGetWord
 from leia.tests.LEIATestCase import LEIATestCase
 from unittest import TestCase
 
 import json
+
+
+class OntoMemEditBufferTestCase(TestCase):
+
+    def setUp(self):
+        self.m = Memory("", "", "")
+
+    def test_note_edited(self):
+        buffer = OntoMemEditBuffer(self.m)
+        self.assertEqual(set(), buffer.edited())
+
+        buffer.note_edited("OBJECT", "DEKADE")
+        self.assertEqual({"OBJECT"}, buffer.edited())
+        self.assertEqual({"OBJECT"}, buffer.edited("DEKADE"))
+        self.assertEqual(set(), buffer.edited("OTHER"))
+
+        buffer.note_edited("EVENT", "OTHER")
+        self.assertEqual({"OBJECT", "EVENT"}, buffer.edited())
+        self.assertEqual({"OBJECT"}, buffer.edited("DEKADE"))
+        self.assertEqual({"EVENT"}, buffer.edited("OTHER"))
+        self.assertEqual(set(), buffer.edited("XYZ"))
+
+        buffer.clear(identifier="EVENT")
+        self.assertEqual({"OBJECT"}, buffer.edited())
+        self.assertEqual({"OBJECT"}, buffer.edited("DEKADE"))
+        self.assertEqual(set(), buffer.edited("OTHER"))
+
+        buffer.clear()
+        self.assertEqual(set(), buffer.edited())
 
 
 class OntoMemTCTPServerTestCase(TestCase):
