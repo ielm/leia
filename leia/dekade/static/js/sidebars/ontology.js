fc4e7e79efbe9aac2299b957736b05a4446ba101
ielm <i.leonmaldonado@gmail.com>
2024-07-19 10:05:07 -0400

    added empty content message to ontology search

--- leia/dekade/static/js/sidebars/ontology.js
+++ leia/dekade/static/js/sidebars/ontology.js
@@ -2,19 +2,43 @@ import * as API from "../api.js";
 import { LEIAObject } from "../objects/_default.js";
 
 
+const enterSearchTemplate = `
+    <div class="empty-contents-message">
+        Enter a search in the field above.
+    </div>
+`;
+
+const noResultsTemplate = `
+    <div class="empty-contents-message">
+        No results found.
+    </div>
+`;
+
+
 $(document).ready(function() {
     $(".ontology-sidebar-filter").on("keyup", _onOntologySidebarFilterKeyup);
+    $(".ontology-sidebar-content").append($(enterSearchTemplate));
 });
 
 
 async function _onOntologySidebarFilterKeyup(event) {
     const filter = $(event.currentTarget).val();
-    const results = await API.apiKnowledgeOntologyFilter(filter);
-
-    const resultsElement = new OntologySidebarResults(results);
-
     const container = $(".ontology-sidebar-content");
     container.empty();
+
+    if (filter == "") {
+        container.append($(enterSearchTemplate));
+        return;
+    }
+
+    const results = await API.apiKnowledgeOntologyFilter(filter);
+
+    if (results.length == 0) {
+        container.append($(noResultsTemplate));
+        return;
+    }
+
+    const resultsElement = new OntologySidebarResults(results);
     container.append(await resultsElement.html());
 }
 
